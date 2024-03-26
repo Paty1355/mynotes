4c7a53965fe2dd048442d98eec1f78a45f1977e9
Paty1355 <94170413+Paty1355@users.noreply.github.com>
2024-03-26 19:47:40 +0100

    Unit Testing

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 @immutable //nie beda sie zmieniac rzeczy w klasie
 class AuthUser{
   final bool isEmailVerified;
-  const AuthUser( this.isEmailVerified);
+  const AuthUser({required this.isEmailVerified});
 
-  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified);
+  factory AuthUser.fromFirebase(User user) => 
+  AuthUser(isEmailVerified: user.emailVerified);//zeby bylo wiadomo co to za bool zmienna
 }
