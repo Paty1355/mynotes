f4bb1c247a4d712e6b05063e5be48a35d0e3648e
Paty1355 <94170413+Paty1355@users.noreply.github.com>
2024-05-27 23:46:49 +0200

    step 12

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -1,11 +1,17 @@
-import 'package:firebase_auth/firebase_auth.dart'  show User;
+import 'package:firebase_auth/firebase_auth.dart' show User;
 import 'package:flutter/material.dart';
 
 @immutable //nie beda sie zmieniac rzeczy w klasie
-class AuthUser{
+class AuthUser {
+  final String? email;
   final bool isEmailVerified;
-  const AuthUser({required this.isEmailVerified});
+  const AuthUser({
+    required this.email,
+    required this.isEmailVerified,
+  });
 
-  factory AuthUser.fromFirebase(User user) => 
-  AuthUser(isEmailVerified: user.emailVerified);//zeby bylo wiadomo co to za bool zmienna
+  factory AuthUser.fromFirebase(User user) => AuthUser(
+        email: user.email,
+        isEmailVerified: user.emailVerified,
+      ); //zeby bylo wiadomo co to za bool zmienna
 }
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:notes/constants/routes.dart';
 import 'package:notes/enums/menu_action.dart';
 import 'package:notes/services/auth/auth_service.dart';
+import 'package:notes/services/crud/notes_service.dart';
 
 class NotesView extends StatefulWidget {
   const NotesView({super.key});
@@ -11,56 +12,98 @@ class NotesView extends StatefulWidget {
 }
 
 class _NotesViewState extends State<NotesView> {
+  late final NotesService _notesService;
+  String get userEmail => AuthService.firebase()
+      .currentUser!
+      .email!; //wykrzykniki bo zmienne nie opcjonalne
+
+  @override
+  void initState() {
+    _notesService = NotesService();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _notesService.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Main UI'),
-        actions: [
-          PopupMenuButton<MenuAction>(onSelected: (value) async {
-            switch(value){
-              case MenuAction.logout:
-                final shouldLogout = await showLogOutDialog(context);
-                if (shouldLogout){
-                  await  AuthService.firebase().logOut();
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    loginRoute, (_) => false);
+        appBar: AppBar(
+          title: const Text('Main UI'),
+          actions: [
+            PopupMenuButton<MenuAction>(
+              onSelected: (value) async {
+                switch (value) {
+                  case MenuAction.logout:
+                    final shouldLogout = await showLogOutDialog(context);
+                    if (shouldLogout) {
+                      await AuthService.firebase().logOut();
+                      Navigator.of(context)
+                          .pushNamedAndRemoveUntil(loginRoute, (_) => false);
+                    }
                 }
+              },
+              itemBuilder: (context) {
+                return [
+                  const PopupMenuItem<MenuAction>(
+                    value: MenuAction.logout,
+                    child: Text("Log out"),
+                  )
+                ];
+              },
+            )
+          ],
+        ),
+        body: FutureBuilder(
+          future: _notesService.getOrCreateUser(email: userEmail),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                return StreamBuilder(
+                  stream: _notesService.allNotes,
+                  builder: (context, snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.waiting:
+                        return const Text('Waiting for all notes..');
+                      default:
+                        return const CircularProgressIndicator();
+                    }
+                  },
+                );
+              default:
+                return const CircularProgressIndicator();
             }
-          },itemBuilder: (context) {
-            return [
-              const PopupMenuItem<MenuAction>(value: MenuAction.logout,
-              child: Text("Log out"),
-             )
-            ];
-           
-          },)
-        ],
-      ),
-      body: const Text("Hello word"),
-    );
+          },
+        ));
   }
 }
-Future<bool> showLogOutDialog(BuildContext context){
+
+Future<bool> showLogOutDialog(BuildContext context) {
   return showDialog<bool>(
-  context: context,
-  builder: (context){
-    return AlertDialog(
-      title: const Text('Sing out'),
-      content: const Text('Are you sure you want to sign out?'),
-      actions: [
-        TextButton(
-          onPressed: (){
-            Navigator.of(context).pop(false);
-          }, child: const Text('Cancel'),
-        ),
-        TextButton(
-          onPressed: (){
-            Navigator.of(context).pop(true);
-          }, child: const Text('Log out'),
-        )
-      ],
-    );
-  },
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Sing out'),
+        content: const Text('Are you sure you want to sign out?'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(false);
+            },
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+            child: const Text('Log out'),
+          )
+        ],
+      );
+    },
   ).then((value) => value ?? false);
 }
